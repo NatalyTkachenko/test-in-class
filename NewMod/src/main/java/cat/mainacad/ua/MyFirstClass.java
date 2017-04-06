9ee73c561e4f1931c3d200efdb59d9776af2666b
NatalyTkachenko <dn-nataly@mail.ru>
2017-04-06 19:29:10 +0300

    first commit

---
+++ NewMod/src/main/java/cat/mainacad/ua/MyFirstClass.java
@@ -0,0 +1,16 @@
+package mainacad.ua;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.util.Scanner;
+
+/**
+ * Created by dn-na on 04/04/2017.
+ */
+public class MyFirstClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        System.out.println(line);
+    }
+}
---
+++ src/main/java/mainacad/FirstProject.java
@@ -0,0 +1,12 @@
+package mainacad;
+
+import java.util.Scanner;
+
+/**
+ * Created by dn-na on 06/04/2017.
+ */
+public class FirstProject {
+    public static void main(String[] args) {
+     System.out.println(2+2);
+    }
+}
