b4093d72a3c25e94c218e9eb63fd95180598c63e
NatalyTkachenko <dn-nataly@mail.ru>
2017-04-11 19:54:07 +0300

    Loops

---
+++ src/main/java/classwork/LoopFor.java
@@ -0,0 +1,19 @@
+package classwork;
+
+/**
+ * Created by dn-na on 11/04/2017.
+ */
+public class LoopFor {
+    public static void main(String[] args) {
+       /* System.out.println("Hello, world");
+        System.out.println("Hello, world");
+        System.out.println("Hello, world");
+        System.out.println("Hello, world");
+        System.out.println("Hello, world");
+        */
+        // Пример с циклом for
+         for (int i=0;i<5;i++){
+             System.out.println("Hello, world");
+         }
+    }
+}
---
+++ src/main/java/classwork/LoopWhile.java
@@ -0,0 +1,21 @@
+package classwork;
+
+/**
+ * Created by dn-na on 11/04/2017.
+ */
+public class LoopWhile {
+    public static void main(String[]args) {
+       /* System.out.println("Hello, world");
+        System.out.println("Hello, world");
+        System.out.println("Hello, world");
+        System.out.println("Hello, world");
+        System.out.println("Hello, world");
+        */
+        // Пример с циклом while:
+        int i = 0;
+        while (i < 5) {
+            System.out.println("Hello, world");
+            i++;
+        }
+    }
+}
